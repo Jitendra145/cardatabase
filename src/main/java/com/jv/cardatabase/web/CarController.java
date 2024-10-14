165459ecba187e374cc977c9dce79b2ddeb8596d
Jitendra145 <jitenverma145@gmail.com>
2024-10-14 14:50:04 +0530

    Initial commit

---
+++ src/main/java/com/jv/cardatabase/web/CarController.java
@@ -0,0 +1,21 @@
+package com.jv.cardatabase.web;
+
+import com.jv.cardatabase.domain.Car;
+import com.jv.cardatabase.domain.CarRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CarController {
+
+    private final CarRepository repository;
+
+    public CarController(CarRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/cars")
+    public Iterable<Car> getCars() {
+        return repository.findAll();
+    }
+}
